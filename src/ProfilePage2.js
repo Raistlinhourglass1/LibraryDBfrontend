7b60fa08f05835697fcf4637861241cbd93d4008
Raistlinhourglass1 <hendrixjustin908@gmail.com>
2024-11-02 11:36:24 -0500

    Changed how far panel goes

--- src/ProfilePage2.js
+++ src/ProfilePage2.js
@@ -174,7 +174,7 @@ export default function ProfilePage2(props) {
         <MuiCard variant="outlined" sx={{ width: '100%', maxWidth: 1200 }}>
           <Paper elevation={0} sx={{ p: 3, bgcolor: 'background.paper', display: 'flex', flexDirection: 'row' }}>
             {/* Left Panel */}
-            <Grid2 item sx={{ width: 250, minWidth: 250, mr: 2 }}>
+            <Grid2 item sx={{ width: 250, minWidth: 200, mr: 2 }}>
               <Paper
                 elevation={1}
                 sx={{
