3d139fc9e3d815952839d9cad4fe96b83ab91be1
nicholas kim <154484441+nicholaskim7@users.noreply.github.com>
2024-11-17 23:14:59 -0600

    Update RoomReserveTable.js "format"

--- src/RoomReserveTable.js
+++ src/RoomReserveTable.js
@@ -86,7 +86,6 @@ const RoomReserveTable = (props) => {
         return renderStatus(status);
       },
     },
-    { field: 'room_number', headerName: 'Room Number', width: 150 },
     {
       field: 'reservation_date',
       headerName: 'Date',
@@ -105,6 +104,8 @@ const RoomReserveTable = (props) => {
         }
       },
     },
+    { field: 'room_number', headerName: 'Room Number', width: 150 },
+    
     { field: 'reservation_reason', headerName: 'Reason', width: 160 },
     { field: 'reservation_duration_hrs', headerName: 'Duration (hrs)', width: 160 },
     { field: 'party_size', headerName: 'Party Size', width: 150 },
